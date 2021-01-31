8d14f89da64b1c49c8fd6315e7d2b6c972c3bcdf
hramazan <ibrahim.ramazan@hotmail.com>
2021-01-31 23:04:02 +0300

    Play Video is done

---
+++ projects/PlayVideo/src/PlayVideo.cpp
@@ -0,0 +1,68 @@
+//============================================================================
+// Name        : PlayVideo.cpp
+// Author      : hir&aco
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+ //open the video file for reading
+ VideoCapture cap("/home/hibo/git/openCV/projects/practiceVideo.mp4");
+ //cap.open("practiceVideo", apiPreference)
+
+ // if not success, exit program
+ if (cap.isOpened() == false)
+ {
+  cout << "Cannot open the video file" << endl;
+  cin.get(); //wait for any key press
+  return -1;
+ }
+
+ //Uncomment the following line if you want to start the video in the middle
+ //cap.set(CAP_PROP_POS_MSEC, 300);
+
+ //get the frames rate of the video
+ double fps = cap.get(CV_CAP_PROP_FPS);
+ cout << "Frames per seconds : " << fps << endl;
+
+ String window_name = "My First Video";
+
+ namedWindow(window_name, WINDOW_AUTOSIZE); //create a window
+
+ while (true)
+ {
+  Mat frame;
+  bool bSuccess = cap.read(frame); // read a new frame from video
+
+  //Breaking the while loop at the end of the video
+  if (bSuccess == false)
+  {
+   cout << "Found the end of the video" << endl;
+   break;
+  }
+
+  //show the frame in the created window
+  imshow(window_name, frame);
+
+  //wait for for 10 ms until any key is pressed.
+  //If the 'Esc' key is pressed, break the while loop.
+  //If the any other key is pressed, continue the loop
+  //If any key is not pressed withing 10 ms, continue the loop
+  if (waitKey(10) == 27)
+  {
+   cout << "Esc key is pressed by user. Stoppig the video" << endl;
+   break;
+  }
+ }
+
+ return 0;
+
+}
